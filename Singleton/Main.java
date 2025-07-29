83f773b06e14d8aab21da8096b43cee73b0dd097
Shaad <141047163+Shaad98@users.noreply.github.com>
2025-07-29 17:01:37 +0530

    Breaking Singleton pattern using Reflection API is done

--- Singleton/Main.java
+++ Singleton/Main.java
@@ -1,12 +1,22 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class Main {
-   
-    public static void main(String[] args) {
-        
+
+    public static void main(String[] args) throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+
         Singleton singleton1 = Singleton.getSingletonObj();
         Singleton singleton2 = Singleton.getSingletonObj();
 
         System.out.println(singleton1.hashCode());
         System.out.println(singleton2.hashCode());
-        
+
+        Constructor<Singleton> constructor = Singleton.class.getDeclaredConstructor();
+        constructor.setAccessible(true); // bypass private access
+
+        Singleton reflectionInstance = constructor.newInstance();
+
+        System.out.println("Reflection instance:");
+        System.out.println(reflectionInstance.hashCode());
     }
 }
