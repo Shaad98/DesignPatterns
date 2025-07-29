6d6fbba2cfa1baa9a256af9a2804396a199fd946
Shaad <141047163+Shaad98@users.noreply.github.com>
2025-07-29 17:06:09 +0530

    Singleton Pattern Prevent from Reflection API

--- Singleton/Singleton.java
+++ Singleton/Singleton.java
@@ -1,7 +1,12 @@
 public class Singleton{
     public static Singleton singleton = new Singleton();
 
-    private Singleton(){}
+    private Singleton(){
+        if(singleton!=null)
+        {
+            throw new RuntimeException("You will not break it using reflection!");
+        }
+    }
 
     public static Singleton getSingletonObj()
     {
